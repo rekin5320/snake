b980c63e05411581280d5cb2f61a159b0b051486
rekin5320 <mmach331@gmail.com>
2022-04-17 21:17:10 +0200

    Don't back up the `version` file.

--- snake.py
+++ snake.py
@@ -330,7 +330,6 @@ class File:  # Data
         try:
             logger.debug("Writing data")
             conf.path_data.replace(conf.path_data_backup)
-            conf.path_version.replace(conf.path_version_backup)
             self.datadict["speed"] = conf.speed
             self.datadict["highscore"] = self.highscore
             self.datadict["highscores_speed"] = self.highscores_speed
@@ -868,7 +867,6 @@ class conf:
     path_data = path_gameDirectory / "data"  # ~/.snake/data
     path_data_backup = path_gameDirectory / "data.backup"  # ~/.snake/data.backup
     path_version = path_gameDirectory / "version"  # ~/.snake/version
-    path_version_backup = path_gameDirectory / "version.backup"  # ~/.snake/version.backup
     path_assetsDirectory = path_gameDirectory / "assets"  # ~/.snake/assets/
     path_font = path_assetsDirectory / "OpenSans-Bold.ttf"
     path_music_Game = path_assetsDirectory / "Tristan Lohengrin - Happy 8bit Loop 01.ogg"
