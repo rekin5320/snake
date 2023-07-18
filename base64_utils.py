85e69a8865665ee4a8c051cfe0e17cf395e46ac9
rekin5320 <mmach331@gmail.com>
2023-07-19 00:23:18 +0200

    Added type hinting to base64 functions

--- base64_utils.py
+++ base64_utils.py
@@ -1,13 +1,13 @@
 import base64
 
 
-def base64_encode(text):
+def base64_encode(text: str) -> str:
     encodedtext_bytes = base64.b64encode(text.encode("utf-8"))
     encodedtext = str(encodedtext_bytes, "utf-8")
     return encodedtext
 
 
-def base64_decode(text):
+def base64_decode(text: str) -> str:
     decodedtext_bytes = base64.b64decode(text)
     decodedtext = str(decodedtext_bytes, "utf-8")
     return decodedtext
